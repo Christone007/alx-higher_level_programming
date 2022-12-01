06fa948d5136ff4412ebebe0086fb6780ba1213c
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-12-01 02:29:04 +0000

    convert negative to positive before modulo

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = number % 10
+mod_number = number
+
+if number < 0:
+    mod_number = number * (-1)
+
+last = mod_number % 10
+
 if last > 5:
     print(f"Last digit of {number} is {last} and is greater than 5")
 elif last == 0:
