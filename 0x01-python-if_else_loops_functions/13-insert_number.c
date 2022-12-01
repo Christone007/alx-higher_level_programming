78cc3e86207cf4514c16126beac8861620333b47
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-12-01 03:43:31 +0000

    Insert node into an ordered list of numbers

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * insert_node - Inserts a number into a sorted linked list
+ * @head: The head of the list
+ * @number: The number to insert
+ *
+ * Return: The address of the new node, NULL if failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *newnode, *start;
+
+	start = *head;
+
+	newnode = malloc(sizeof(listint_t));
+	newnode->n = number;
+	newnode->next = NULL;
+
+	if (start == NULL)
+		return (NULL);
+
+	if (start->next == NULL)
+	{
+		start->next = newnode;
+		return (newnode);
+	}
+
+	while (start->next != NULL)
+	{
+		if (start->next->n >= number)
+		{
+			newnode->next = start->next;
+			start->next = newnode;
+			return (newnode);
+		}
+
+		start = start->next;
+	}
+
+	return (NULL);
+}
