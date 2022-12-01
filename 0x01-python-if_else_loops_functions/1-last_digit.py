fe6575d71e12de4d44d29eb814ebceaa3729f0f8
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-12-01 02:32:38 +0000

    Add negative sign

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,11 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 mod_number = number
+last = 1
 
 if number < 0:
     mod_number = number * (-1)
+    last = -1
 
-last = mod_number % 10
+last = last * (mod_number % 10)
 
 if last > 5:
     print(f"Last digit of {number} is {last} and is greater than 5")
