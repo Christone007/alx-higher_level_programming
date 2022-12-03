407f14f71d9fa3ab8f18fb02f32df6161e56c4d7
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-12-03 07:41:09 +0000

    Write a dynamic script

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import sys
+
+
+def myfunc(argument):
+    argc = len(argument) - 1
+    print("{:d} ".format(argc), end="")
+    if argc == 1:
+        print("argument", end="")
+    else:
+        print("arguments", end="")
+
+    if argc == 0:
+        print(".")
+    else:
+        print(":")
+        for i in range(1, argc + 1):
+            print("{:d}: {:s}".format(i, argument[i]))
+
+if __name__ == "__main__":
+    myfunc(sys.argv)
+
