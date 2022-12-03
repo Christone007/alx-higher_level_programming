e5800ceff32d3b0175f974cd9c3501854e2d4af9
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-12-03 08:00:16 +0000

    Fix linter errors

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,10 +5,8 @@ import sys
 def myfunc(argument):
     sum = 0
     for i in range(1, len(argument)):
-        sum +=int(argument[i])
-
+        sum += int(argument[i])
     print("{:d}".format(sum))
-        
 
 
 if __name__ == "__main__":
