314ed8bc10d0718105dbd35e7cbecd71de213e0e
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-12-03 17:53:26 +0000

    Read a compiled module

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import hidden_4
+
+
+def myfunc():
+    names = dir(hidden_4)
+    for name in names:
+        if name[0] != "_" and name[1] != "_":
+            print(name)
+
+
+if __name__ == "__main__":
+    myfunc()
