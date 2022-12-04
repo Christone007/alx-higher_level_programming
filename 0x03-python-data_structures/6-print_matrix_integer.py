001fbc0fe0051c8121198db8d82be155dcfaaaa0
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-12-04 05:45:03 +0000

    Apply integer conversion

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,9 +5,8 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(length):
         if breadth == 0:
             print()
-            return
         for j in range(breadth):
             if j == breadth - 1:
-                print("{}".format(matrix[i][j]))
+                print("{:d}".format(matrix[i][j]))
             else:
-                print("{}".format(matrix[i][j]), end=" ")
+                print("{:d}".format(matrix[i][j]), end=" ")
