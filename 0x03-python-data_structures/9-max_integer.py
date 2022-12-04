ae483259bc797e729de72917c617ad2e2a5063ab
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-12-04 06:49:34 +0000

    Get max number

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    length = len(my_list)
+    if length == 0:
+        return None
+    i = 0;
+    j = i + 1;
+
+    while j < length:
+        if my_list[i] > my_list[j]:
+            tmp = my_list[i]
+            my_list[i] = my_list[j]
+            my_list[j] = tmp
+        j += 1
+        i += 1
+    return my_list[length - 1]
