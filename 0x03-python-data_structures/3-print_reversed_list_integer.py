657a0dbdbe1581d975f3422a69c0c197b581b14d
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-12-08 12:59:52 +0000

    Handle empty list in reverse print

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     count = len(my_list)
+    if count == 0:
+        return
     for i in range(count - 1, -1, -1):
         print("{:d}".format(my_list[i]))
+
+print_reversed_list_integer();
