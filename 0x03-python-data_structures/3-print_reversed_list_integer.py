21c0625ababfc215685d968e88ec4ed13d481cc8
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-12-08 13:13:32 +0000

    Fix error for None as input

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,5 +5,3 @@ def print_reversed_list_integer(my_list=[]):
     count = len(my_list)
     for i in range(count - 1, -1, -1):
         print("{:d}".format(my_list[i]))
-
-print_reversed_list_integer(None);
