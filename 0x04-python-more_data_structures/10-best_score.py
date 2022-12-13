eb5d4d03b4b2400ae92547643968cd3a48fab169
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-12-13 01:38:19 +0000

    Resolve file issues

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,5 @@
 def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary) == 0:
         return None
-    sorted_list = (sorted(a_dictionary.items(), key = lambda x:x[1]))
+    sorted_list = (sorted(a_dictionary.items(), key=lambda x: x[1]))
     return (sorted_list[len(sorted_list) - 1][0])
