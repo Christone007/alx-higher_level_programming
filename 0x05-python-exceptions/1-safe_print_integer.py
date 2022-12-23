dedb44c558274433f83538ca018ac0069d265d11
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-12-23 16:41:06 +0000

    Correct code style

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
+
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
     except ValueError:
         return False
-    except:
+    except Exception:
         return False
     else:
         return True
