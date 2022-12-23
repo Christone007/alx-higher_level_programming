f2afe223657f0f996e6e508702da3e161ddecea7
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-12-23 17:09:44 +0000

    Correct code style

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
     while i < x:
         try:
             print("{:d}".format(my_list[i]), end="")
-            count = count + 1 
+            count = count + 1
         except (ValueError, TypeError):
             pass
         finally:
