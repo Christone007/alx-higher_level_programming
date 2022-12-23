5ba957b5a3e316ff17729089ff30de62485c2d05
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-12-23 17:33:51 +0000

    Divide two lists

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    i = 0
+    result = []
+    while i < list_length:
+        try:
+            div = my_list_1[i] / my_list_2[i]
+            result.append(div)
+        except (TypeError, ValueError):
+            print("wrong type")
+            result.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            result.append(0)
+        except IndexError:
+            print("out of range")
+            result.append(0)
+        finally:
+            i = i + 1
+    return result
