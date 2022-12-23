45b517018acdc4fb0e0985e79a135c2168d0be76
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-12-23 15:40:57 +0000

    Remove semicolon

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         while count < x:
             print("{}".format(my_list[count]), end="")
-            count = count + 1;
+            count = count + 1
 
         print()
         return count
