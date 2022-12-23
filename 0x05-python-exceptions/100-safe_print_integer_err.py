b108ca47d8378a2bb0db930cbe65b2b3f53b9f07
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-12-23 18:03:13 +0000

    Correct style error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,8 +7,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except Exception as e:
-        print("Exception: {}".format(e), file = sys.stderr)
+        print("Exception: {}".format(e), file=sys.stderr)
         return False
     else:
         return True
-
