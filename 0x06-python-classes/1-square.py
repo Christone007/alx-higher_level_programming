33381f278f20ccf09a74e3406e373d04e6c93d8f
Nwaburu Emeka Christian <exellentemy@gmail.com>
2022-12-27 06:47:16 +0000

    Add a private instance variable

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This is a Module to demonstrate the creatiion
+of classes in python
+"""
+
+
+class Square:
+    """This is an empty square class.
+
+    The class has no attributes
+    """
+    def __init__(self, size):
+        self.__size = size
