06f4b893878dc31b36e6d964ab176fd155d9794a
Nwaburu Emeka Christian <exellentemy@gmail.com>
2023-01-03 22:49:53 +0000

    Refactor docs

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Defines an integer addition function."""
+"""Defines an integer addition function.
+Example:
+    >>> add_integer(4, 5)
+    9
+"""
 
 
 def add_integer(a, b=98):
     """Return the integer addition of a and b.
-
-    Float arguments are typecasted to ints before addition is performed.
-
-    Raises:
-        TypeError: If either of a or b is a non-integer and non-float.
+    Floats must be typecasted to integer before use
     """
     if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
