98bdd782d06b117e4cb57074d39743647fc9d167
Nwaburu Emeka Christian <exellentemy@gmail.com>
2023-01-04 15:02:21 +0000

    Correct output string

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -7,7 +7,7 @@ def say_my_name(first_name, last_name=""):
 
     ::
         >>> say_my_name("Emeka", "Nwaburu")
-        Emeka Nwaburu
+        My name is Emeka Nwaburu
     """
     first_name = first_name
     last_name = last_name
@@ -18,4 +18,4 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    print("{} {}".format(first_name, last_name))
+    print("My name is {} {}".format(first_name, last_name))
