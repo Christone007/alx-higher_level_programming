8f1f7cc7181cf19cbe9a82352e27f8c9e858afcd
Nwaburu Emeka Christian <exellentemy@gmail.com>
2023-01-04 21:11:07 +0000

    created more tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,9 +8,23 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     def test_max(self):
-        self.assertAlmostEqual(max_integer([6, 4, 2, 10]), 10)
+        self.assertAlmostEqual(max_integer([6, 4, 10, 1]), 10)
+    
+    def test_max_at_end(self):
         self.assertAlmostEqual(max_integer([-2, 4, -10, 5]), 5)
+        self.assertAlmostEqual(max_integer([-15, -8,- 11, -7]), -7)
+
+    def test_max_at_start(self):
+        self.assertAlmostEqual(max_integer([6, 2, 5]), 6)
+        self.assertAlmostEqual(max_integer([-5, -12, -20]), -5)
+
+    def test_max_in_middle(self):
+        self.assertAlmostEqual(max_integer([6, 7, 2]), 7)
+        self.assertAlmostEqual(max_integer([-2, 0, -1]), 0)
+
+    def test_single_element(self):
         self.assertAlmostEqual(max_integer([6]), 6)
+        self.assertAlmostEqual(max_integer([-7]), -7)
 
     def test_empty(self):
         self.assertAlmostEqual(max_integer([]), None)
