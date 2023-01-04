c36bfeefb62a278f6170c555a428d333e83b9b4b
Nwaburu Emeka Christian <exellentemy@gmail.com>
2023-01-04 04:52:58 +0000

    Create doctests for say_my_name function

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module defines a function that prints a name"""
+
+
+def say_my_name(first_name, last_name=""):
+    """This function simply prints a fullname
+
+    ::
+        >>> say_my_name("Emeka", "Nwaburu")
+        Emeka Nwaburu
+    """
+    first_name = first_name
+    last_name = last_name
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("{} {}".format(first_name, last_name))
