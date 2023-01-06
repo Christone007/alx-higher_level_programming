b8fadf8e1530fd9c32594180340c5a2f1d09954a
Nwaburu Emeka Christian <exellentemy@gmail.com>
2023-01-06 02:37:09 +0000

    properly refrence class attributes

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -109,12 +109,12 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
     def __del__(self):
         print("Bye rectangle...")
-        if number_of_instances > 0:
-            number_of_instances -= 1
+        if Rectangle.number_of_instances > 0:
+            Rectangle.number_of_instances -= 1
 
     @property
     def width(self):
