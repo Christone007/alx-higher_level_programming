0068f755c53618e68fd2dcc30555b50fb32e5815
Nwaburu Emeka Christian <exellentemy@gmail.com>
2023-01-07 04:41:23 +0000

    document code

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""This is a class that accepts only one attribute"""
+
+
 class LockedClass:
+    """The __slots__ attribute is defined to prevent
+    the creation of a __dict__ attribute. 
+
+    Hence, the user of this program can only create
+    objects with only one attribute `first_name`
+    """
     __slots__ = 'first_name'
