aa225ee371bfd65cdb07630b38732536a075bab9
Nwaburu Emeka Christian <exellentemy@gmail.com>
2023-11-30 04:44:45 +0000

    Copy code for inserting list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,44 @@
+/**
+ * insert_node - Inserts a number into a sorted linked list
+ * @head: The head of the list
+ * @number: The number to insert
+ *
+ * Return: The address of the new node, NULL if failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+        listint_t *newnode, *ptr;
+
+        ptr = *head;
+
+        if (ptr == NULL)
+                return (NULL);
+
+        newnode = malloc(sizeof(listint_t));
+        if (newnode == NULL)
+                return NULL;
+        newnode->n = number;
+        newnode->next = NULL;
+
+        if(ptr->n >= number)
+        {
+                newnode->next = ptr;
+                head = &newnode;
+                return (newnode);
+        }
+
+        while (ptr->next != NULL)
+        {
+                if (ptr->next->n >= number)
+                {
+                        newnode->next = ptr->next;
+                        ptr->next = newnode;
+                        return (newnode);
+                }
+
+                ptr = ptr->next;
+        }
+
+        ptr->next = newnode;
+        return (newnode);
+}
