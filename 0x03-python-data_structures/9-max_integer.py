dd1080fb7f291892cd65a4b27c8c9287a845501c
Nwaburu Emeka Christian <exellentemy@gmail.com>
2023-12-01 15:32:39 +0000

    Write a function to find the max integer in a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+    """Returns the maximum integer"""
+    
+    if my_list == []:
+        return None
+    else:
+        max = 0
+        for i in my_list:
+            if i > max:
+                max = i
+        return max
