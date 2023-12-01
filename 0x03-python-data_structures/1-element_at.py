818558df04f5b1f6fb748d31c097f38a3233b96a
Nwaburu Emeka Christian <exellentemy@gmail.com>
2023-12-01 13:30:14 +0000

    Write function to replace single element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,11 +4,10 @@
 def element_at(my_list, idx):
     """Returns an element at specified index"""
 
-    if my_list == None or len(my_list) == 0:
+    if my_list is None or len(my_list) == 0:
         return None
     elif idx < 0:
         return None
     elif idx > len(my_list) - 1:
         return None
     return my_list[idx]
-
