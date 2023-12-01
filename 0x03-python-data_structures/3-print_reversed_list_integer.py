c1436f215d0c035d8ab7e71516436d0df5c28ba9
Nwaburu Emeka Christian <exellentemy@gmail.com>
2023-12-01 13:30:51 +0000

    Write a function to print a list in reverse

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def print_reversed_list_integer(my_list=[]):
+    """Print a list in reverse"""
+
+    if my_list is not None or len(my_list) > 1:
+        temp_list = my_list.copy()
+        temp_list.reverse()
+        
+        for i in temp_list:
+            print("{:d}".format(i))
+    else:
+        print(my_list)
