613dc67f12a2ba69f72579d790950c9922697543
Nwaburu Emeka Christian <exellentemy@gmail.com>
2023-12-04 14:59:56 +0000

    Cast to int in a correct way

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -8,6 +8,6 @@ def print_reversed_list_integer(my_list=[]):
         my_list.reverse()
 
         for i in my_list:
-            print("{:d}".format(str(i)))
+            print("{:d}".format(int(i)))
     else:
         print(my_list)
