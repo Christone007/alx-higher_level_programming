85c97ea0c4d888f09ceb63b24260cd717692717c
Nwaburu Emeka Christian <exellentemy@gmail.com>
2023-12-04 16:34:46 +0000

    Set the starting point to first element instead of 0

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -8,9 +8,11 @@ def max_integer(my_list=[]):
         if my_list == []:
             return None
         else:
-            max = 0
+            max = my_list[0]
             for i in my_list:
+
                 if i > max:
                     max = i
             return max
-    return None
+    else:
+        return None
