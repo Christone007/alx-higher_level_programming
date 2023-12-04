9ec656d4734a70a8b197cc993e748063e614afb2
Nwaburu Emeka Christian <exellentemy@gmail.com>
2023-12-04 15:24:44 +0000

    Treat unary list as other lists

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -6,10 +6,8 @@ def print_reversed_list_integer(my_list=[]):
 
     if my_list == []:
         print(my_list)
-    elif len(my_list) == 1:
-        print(my_list)
     else:
         my_list.reverse()
 
         for i in my_list:
-            print("{:d}".format(i))
+            print("{}".format(int(i)))
