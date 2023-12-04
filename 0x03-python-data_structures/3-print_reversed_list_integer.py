d9be7d6358e8111b7f6f93635521b83621002a73
Nwaburu Emeka Christian <exellentemy@gmail.com>
2023-12-04 16:09:42 +0000

    Return if list is empty

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,6 +4,8 @@
 def print_reversed_list_integer(my_list=[]):
     """Print a list in reverse"""
 
+    if my_list == []:
+        return
     my_list.reverse()
     for i in my_list:
         print("{:d}".format(i))
