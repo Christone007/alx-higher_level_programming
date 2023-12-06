3413a966cd03fb6c96fd37cc7a001d7de7ed828d
Nwaburu Emeka Christian <exellentemy@gmail.com>
2023-12-06 11:26:30 +0000

    Cover edge cases

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -4,6 +4,11 @@
 def complex_delete(a_dictionary, value):
     """Deletes keys with specific values"""
 
+    if a_dictionary is None:
+        return None
+    elif value is None:
+        return a_dictionary
+
     new_dictionary = a_dictionary.copy()
 
     for k, v in new_dictionary.items():
