6cc496397d73490aa4d163857a4c637776e5acf4
Nwaburu Emeka Christian <exellentemy@gmail.com>
2023-12-06 04:53:13 +0000

    Use the Python.h header file instead

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,5 @@
+#include <Python.h>
 #include <stdio.h>
-#include <listobject.h>
-#include <object.h>
 
 /**
  * print_python_list_info - Prints certain info about a list
@@ -10,18 +9,17 @@
  */
 void print_python_list_info(PyObject *p)
 {
-	PyListObject *list = (PyListObject *)p;
+    PyListObject *list = (PyListObject *)p;
 
-	Py_ssize_t size = Py_SIZE(list);
-	Py_ssize_t allocated = list->allocated;
+    Py_ssize_t size = Py_SIZE(list);
+    Py_ssize_t allocated = list->allocated;
 
-	printf("[*] Size of the Python List = %zd\n", size);
-	printf("[*] Allocated = %zd\n", allocated);
+    printf("[*] Size of the Python List = %zd\n", size);
+    printf("[*] Allocated = %zd\n", allocated);
 
-	for (Py_ssize_t i = 0; i < size; ++i)
-	{
-		PyObject *element = list->ob_item[i];
-
-		printf("Element %zd: %s\n", i, Py_TYPE(element)->tp_name);
-	}
+    for (Py_ssize_t i = 0; i < size; ++i)
+    {
+        PyObject *element = list->ob_item[i];
+        printf("Element %zd: %s\n", i, Py_TYPE(element)->tp_name);
+    }
 }
