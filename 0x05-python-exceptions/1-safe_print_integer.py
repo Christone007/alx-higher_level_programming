a3cf191e3cc4ce19d1072f2a86d8a69695607a05
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-08-14 11:22:47 +0100

    Print integer safely

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """Safely prints an integer value"""
+
+    try:
+        print("{:d}".format(value))
+    except:
+        return False
+    else:
+        return True
