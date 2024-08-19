33b7c1083906b4b5b59b9cebe9db789d58035bcf
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-08-19 16:06:35 +0100

    Create a function that returns the area of the square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""The Square Module
+
+This module documents a class that simply creates a square
+
+"""
+
+
+class Square:
+    """This class defines a square
+    """
+
+    def __init__(self, size=0):
+        """Initializes the square
+
+        Args:
+            size: The length of the square
+        """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Calculates the area of the square
+
+        Returns:
+            The area of the square
+        """
+        return self.__size * self.__size
