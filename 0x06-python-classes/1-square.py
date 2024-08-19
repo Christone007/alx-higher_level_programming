f838c0796097f51fbb9e621ee43d5be32fc787d5
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-08-19 15:22:08 +0100

    Create a square class with a private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""The Square Module
+
+This module documents a class that simply creates a square
+
+"""
+
+
+class Square:
+    """This class defines a square
+    """
+
+    def __init__(self, size):
+        """Initializes the square
+
+        Args:
+            size: The length of the square
+        """
+
+        self.__size = size
