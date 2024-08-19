6d11e2bd32a6673aeb13687355c9a9621027c5ec
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-08-19 15:01:01 +0100

    Document module and class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
 
+"""The Square Module
+
+This module documents a class that simply creates a square
+
+Description:
+    This is the base module that defines the empty class
+"""
+
 class Square:
+    """This class defines the empty square
+
+    It has no attributes
+    """
     pass
