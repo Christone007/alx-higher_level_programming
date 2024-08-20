af5adaa1907de93c9e2ab226918508bdccfd48b0
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-08-20 11:06:15 +0100

    Correct Magic code errors

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -2,12 +2,14 @@
 
 """Translated manually from pytgon bytecode"""
 
+
 class MagicClass:
     """The magic class"""
-    
+
     def __init__(self, radius):
+        self.__radius = 0
         if (
-                type(radius) is not int or
+                type(radius) is not int and
                 type(radius) is not float
                 ):
             raise TypeError("radius must be a number")
@@ -16,8 +18,8 @@ class MagicClass:
     def area(self):
         """Returns the area of the circle"""
 
-        return self.__radius ** 2 * math(pi)
+        return self.__radius ** 2 * math.pi
 
     def circumference(self):
         """Returns the circumference of the circle"""
-        return math(pi) * 2 * self.__radius
+        return math.pi * 2 * self.__radius
