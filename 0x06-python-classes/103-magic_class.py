0a6c706a045ffd3b44c38d2acc07fc4d5e0a492f
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-08-20 11:19:13 +0100

    Add default value

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -6,7 +6,7 @@
 class MagicClass:
     """The magic class"""
 
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         self.__radius = 0
         if (
                 type(radius) is not int and
