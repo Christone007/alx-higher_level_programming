7ffa5c6fd49157f593f11adb56de88be8a4b4e3c
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-08-21 05:47:44 +0100

    Test the addition function

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""The add integer module"""
+
+def add_integer(a, b=98):
+    """Adds two integers and returns an integer"""
+    if not(
+            isinstance(a, int) or
+            isinstance(a, float)
+            ):
+        raise TypeError("a must be an integer")
+    if not(
+            isinstance(b, int) or
+            isinstance(b, float)
+            ):
+        raise TypeError("b must be an integer")
+    if a is None or b is None:
+        raise ValueError
+    
+    return int(a) + int(b)
