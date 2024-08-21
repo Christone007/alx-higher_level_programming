b75ff6d9ecccb9d9032e9313a67d91fa32d966f7
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-08-21 06:01:57 +0100

    Fix linting error

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,19 +2,20 @@
 
 """The add integer module"""
 
+
 def add_integer(a, b=98):
     """Adds two integers and returns an integer"""
-    if not(
+    if not (
             isinstance(a, int) or
             isinstance(a, float)
             ):
         raise TypeError("a must be an integer")
-    if not(
+    if not (
             isinstance(b, int) or
             isinstance(b, float)
             ):
         raise TypeError("b must be an integer")
     if a is None or b is None:
         raise ValueError
-    
+
     return int(a) + int(b)
