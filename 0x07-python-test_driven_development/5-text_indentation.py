068a8cb1b3e586d1c1fd1806a80c45381089d3c3
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-08-24 22:17:05 +0100

    Write a function to format text

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""This module defines a function that formats text
+"""
+
+
+def text_indentation(text):
+    """This function processes the text by adding 2
+    blank lines after every '.', '?' and ':'
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    i = 0
+    while i < len(text):
+        print("{}".format(text[i]), end="")
+
+        if text[i] in (".", "?", ":"):
+            print("\n\n", end="")
+            i += 2
+        else:
+            i += 1
