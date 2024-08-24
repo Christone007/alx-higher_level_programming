282f42492e07ca9bb1845d8d282f5d522d9562bd
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-08-24 11:11:06 +0100

    Add docstrings to the function

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -6,6 +6,14 @@ it was given
 
 
 def say_my_name(first_name, last_name=""):
+    """This function prints the given name
+    Args:
+        first_name(string): The first name
+        last_name(string): The last name, optional
+
+    Returns:
+        Returns nothing
+    """
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
