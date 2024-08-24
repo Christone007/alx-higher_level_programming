683b1ffd000f65ee5c20783abffc9d24be92afcf
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-08-24 09:50:02 +0100

    Add new test cases

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -2,8 +2,8 @@
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
 matrix = [
-    [1, 2, 3],
-    [4, 5, 6]
+    [40, 2, 3],
+    [4, 24, 6]
 ]
-print(matrix_divided(matrix, 3))
+print(matrix_divided(matrix, float('inf')))
 print(matrix)
