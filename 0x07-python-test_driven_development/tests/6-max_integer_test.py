59f6e31bc5f9687d2bdea12bfc49beb003aab350
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-08-25 13:28:11 +0100

    Refactor unittest module for max_integer function

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -12,11 +12,24 @@ class TestMaxInteger(unittest.TestCase):
     function max_integer
     """
 
-    def test_max_integer(self):
+    def test_empty_list(self):
         self.assertEqual(max_integer([]), None)
+
+    def test_max_at_end(self):
         self.assertEqual(max_integer([2, 3]), 3)
-        self.assertEqual(max_integer([-2, 0, 5, -10]), 5)
+
+    def test_all_equal(self):
         self.assertEqual(max_integer([4, 4, 4]), 4)
-        self.assertEqual(max_integer([0, 0]), 0)
         self.assertEqual(max_integer([6]), 6)
+
+    def test_negatives_and_positives(self):
+        self.assertEqual(max_integer([-2, 0, 5, -10]), 5)
+       
+    def test_zeros(self):
+        self.assertEqual(max_integer([0, 0]), 0)
+
+    def test_all_negatives(self):
         self.assertEqual(max_integer([-20, -4, -5, -10]), -4)
+
+    def test_max_at_beginning(self):
+        self.assertEqual(max_integer([10, 3, 9, -2]), 10)
