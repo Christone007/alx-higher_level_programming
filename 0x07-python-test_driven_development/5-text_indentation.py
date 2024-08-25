49350d5d5bfed8d6d8524532851ee95fb7677460
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-08-25 07:51:15 +0100

    Make code aware of multiple spaces

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -17,6 +17,6 @@ def text_indentation(text):
 
         if text[i] in (".", "?", ":"):
             print("\n")
-            if text[i + 1] == " ":
+            while text[i + 1] == " ":
                 i += 1
         i += 1
