2ddb894c795a9afce8e2733ed863bc9f9aa11b5f
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-08-26 21:17:17 +0100

    Fix linter error

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -16,13 +16,13 @@ def matrix_mul(m_a, m_b):
         The product of the 2 matrices
     """
 
-    #variables
+    # variables
     rows_a = 0
     rows_b = 0
     cols_a = 0
     cols_b = 0
 
-    #prelimary validation of both matrices
+    # prelimary validation of both matrices
     if type(m_a) is not list:
         raise TypeError("m_a must be a list")
     if type(m_b) is not list:
@@ -31,14 +31,14 @@ def matrix_mul(m_a, m_b):
         raise ValueError("m_a can't be empty")
     if len(m_b) == 0:
         raise ValueError("m_b can't be empty")
-   
-    #validate matrix a
+
+    # validate matrix a
     if len(m_a) > 0:
-       if type(m_a[0]) is not list:
-           raise TypeError("m_a must be a list of lists")
-       if len(m_a) == 1 and len(m_a[0]) == 0:
-           raise ValueError("m_a can't be empty")
-       cols_a = len(m_a[0])
+        if type(m_a[0]) is not list:
+            raise TypeError("m_a must be a list of lists")
+        if len(m_a) == 1 and len(m_a[0]) == 0:
+            raise ValueError("m_a can't be empty")
+        cols_a = len(m_a[0])
 
     for row_a in m_a:
         if type(row_a) is not list:
@@ -50,7 +50,7 @@ def matrix_mul(m_a, m_b):
                 raise TypeError("m_a should contain only integers or floats")
         rows_a += 1
 
-    #validate matrix b
+    # validate matrix b
     if len(m_b) > 0:
         if type(m_b[0]) is not list:
             raise TypeError("m_b must be a list of lists")
@@ -68,13 +68,13 @@ def matrix_mul(m_a, m_b):
                 raise TypeError("m_b should contain only integers or floats")
         rows_b += 1
 
-    #check if both matrices can be multiplied
+    # check if both matrices can be multiplied
     if cols_a != rows_b:
         raise ValueError("m_a and m_b can't be multiplied")
 
-    #multiply matrices
+    # multiply matrices
     matrix = []
-    
+
     for i in range(rows_a):
         result = []
         for j in range(cols_b):
