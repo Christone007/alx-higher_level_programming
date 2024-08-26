d6dcf91564ac284e56a499be618098c06f367404
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-08-26 04:00:46 +0100

    Create matrix multiplication function

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+
+"""This module contains a function that multiplies
+matrices
+"""
+
+
+def matrix_mul(m_a, m_b):
+    """This function multiplies two matrices
+
+    Args:
+        m_a: The first matrix
+        m_b: The second matrix
+
+    Returns:
+        The product of the 2 matrices
+    """
+
+    #variables
+    rows_a = 0
+    rows_b = 0
+    cols_a = 0
+    cols_b = 0
+
+    #prelimary validation of both matrices
+    if type(m_a) is not list:
+        raise TypeError("m_a must be a list")
+    if type(m_b) is not list:
+        raise TypeError("m_b must be a list")
+    if len(m_a) == 0:
+        raise ValueError("m_a can't be empty")
+    if len(m_b) == 0:
+        raise ValueError("m_b can't be empty")
+   
+   #validate matrix a
+   if len(m_a) > 0:
+       if type(m_a[0]) is not list:
+           raise TypeError("m_a must be a list of lists")
+       if len(m_a) == 1 and len(m_a[0]) == 0:
+           raise ValueError("m_a can't be empty")
+       cols_a = len(m_a[0])
+
+    for row_a in m_a:
+        if type(row_a) is not list:
+            raise TypeError("m_a must be a list of lists")
+        if len(row_a) != cols_a:
+            raise TypeError("each row of m_a must be of the same size")
+        for elem_a in row_a:
+            if type(elem_a) not in (int, float):
+                raise TypeError("m_a should contain only integers or floats")
+        rows_a += 1
+
+    #validate matrix b
+    if len(m_b) > 0:
+        if type(m_b[0]) is not list:
+            raise TypeError("m_b must be a list of lists")
+        if len(m_b) == 1 and len(m_b[0]) == 0:
+            raise ValueError("m_b can't be empty")
+        cols_b = len(m_b[0])
+
+    for row_b in m_b:
+        if type(row_b) is not list:
+            raise TypeError("m_b must be a list of lists")
+        if len(row_b) != cols_b:
+            raise TypeError("each row of m_b must be of the same size")
+        for elem_b in row_b:
+            if type(elem_b) not in (int, float):
+                raise TypeError("m_b should contain only integers or floats")
+        rows_b += 1
+
+    #check if both matrices can be multiplied
+
+    #multiply matrices
+
