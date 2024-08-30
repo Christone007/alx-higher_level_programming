8a4eafc7e3e31807686085eece2271a47803bce2
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-08-30 15:25:01 +0100

    Coly working ckrd from a repo and see

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 
-"""Creates a Locked Class
+"""This module defines a locked class
 """
 
 
-class LockedClass():
-"""Creates a locked class that does not permit creation of new attributes other than the first_name attribute
-"""
-
-    def __setattr__(self, name, value):
-        if name == "first_name":
-            super().__setattr__(name, value)
-
-        return getattr(self, name)
+class LockedClass:
+    """Restricts objects from creating extra attributes
+    """
+    __slot__ = ["first_name"]
