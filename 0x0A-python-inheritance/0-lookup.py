13eee817e9e2f83e111bb198dead7fe1cc0b6243
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-08-31 21:32:09 +0100

    Fix limter error

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
-"""This module has a function that prints all omethods and attributes of objects
+"""This module has a function that prints all
+methods and attributes of objects
 """
+
 
 def lookup(obj):
     """Lists all methods and attributes"""
