9db93395a20f19afe2148c517d9ff7a6138dd36e
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-09-02 07:09:13 +0100

    Check if an object belongs to a class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Checks if an object is an instance of a class"""
+
+def is_same_class(obj, a_class):
+    """Checks if an object belongs to a class"""
+
+    return (type(obj) == a_class)
+
