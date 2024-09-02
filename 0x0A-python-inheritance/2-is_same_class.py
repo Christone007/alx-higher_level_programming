7cb31fe36c277424e427a4c87ef60c053901911d
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-09-02 07:13:16 +0100

    Fix linter errors

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,8 +2,8 @@
 
 """Checks if an object is an instance of a class"""
 
+
 def is_same_class(obj, a_class):
     """Checks if an object belongs to a class"""
 
-    return (type(obj) == a_class)
-
+    return (type(obj) is a_class)
