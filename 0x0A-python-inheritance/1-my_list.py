feac7653618e1cd11fa133daff9acb3ed08a2a4c
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-09-02 06:44:38 +0100

    Understand task better

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,8 +5,12 @@
 
 class MyList(list):
     """A class derived from the default list class"""
+    def __init__(self):
+        """initializes the object"""
+
+        super().__init__()
 
     def print_sorted(self):
         """Prints a sorted list"""
 
-        return self.sort()
+        print(sorted(self))
