c812e52dcba6837f9912a4565ee4db29d36fcae2
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-09-02 14:30:45 +0100

    Define an area method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Declares a Geometry class"""
+
+
+class BaseGeometry:
+    """Initializes a Geometry class"""
+
+    def area(self):
+        """Define an area"""
+
+        raise Exception("area() is not implemented")
