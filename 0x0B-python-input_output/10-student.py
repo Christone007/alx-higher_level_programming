254a37721b3741638ddc4984b096647ea6a4bb91
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-09-03 16:11:31 +0100

    Add only specified elements to object dict representation

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""This Module defines a class that has a method to
+generate a serializable object representation
+"""
+
+
+class Student:
+    """A Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a student object"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns a json serializable object representation"""
+        my_dict = {}
+        if type(attrs) is list:
+            for attr in attrs:
+                if type(attr) is str and attr in self.__dict__:
+                    my_dict[attr] = self.__dict__[attr]
+        else:
+            my_dict = self.__dict__
+
+        return my_dict
