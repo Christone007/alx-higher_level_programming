f9ef19c932772f876dd6ad081b2bb32c6b73a566
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-09-03 13:50:40 +0100

    Load from file, modify and save json

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""This Module adds arguments to a list and then serializes it"""
+
+import sys
+import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+def main(args):
+    filename = "add_item.json"
+
+    try:
+        obj_list = load_from_json_file(filename)
+    except FileNotFoundError:
+        obj_list = []
+    
+    obj_list += args
+    save_to_json_file(obj_list, filename)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
