010b49608cbd747bbfb0bcf3cf80f15f62cd005c
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-09-06 17:31:17 +0100

    Fix long line issue

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -124,7 +124,8 @@ class Rectangle(Base):
     def __str__(self):
         """Returns a printable string representation"""
 
-        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+        return f"[Rectangle] ({self.id}) {self.__x}/\
+                {self.__y} - {self.__width}/{self.__height}"
 
     def update(self, *args, **kwargs):
         """Updates the rectangle"""
