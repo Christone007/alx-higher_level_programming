646fca0a2244ea4797a43b66f4a5acc9cc0edcc0
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-09-06 18:22:42 +0100

    Return the dictionary statically

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -147,4 +147,4 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """Returns a dictionary representation of a rectangle object"""
-        return self.__dict__
+        return ({"id": self.id, "width":self.width, "height":self.height, "x": self.x, "y":self.y})
