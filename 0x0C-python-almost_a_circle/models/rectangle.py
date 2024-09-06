99d5b67744f9407657e918f2185d887e4a85cff7
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-09-06 17:39:13 +0100

    Break line properly

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -124,8 +124,8 @@ class Rectangle(Base):
     def __str__(self):
         """Returns a printable string representation"""
 
-        return f"[Rectangle] ({self.id}) {self.__x}/\
-                {self.__y} - {self.__width}/{self.__height}"
+        return (f"[Rectangle] ({self.id}) {self.__x}"
+                f"/{self.__y} - {self.__width}/{self.__height}")
 
     def update(self, *args, **kwargs):
         """Updates the rectangle"""
