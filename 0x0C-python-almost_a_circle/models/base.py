45b72bf258d175ae09e82ce74e529299c12ccfac
Nwaburu Emeka Christian <exellentemy@gmail.com>
2024-09-06 05:20:42 +0100

    Create a rectangle class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+This module creates a base class
+"""
+
+class Base:
+    """
+    The Base Class
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id != None:
+            self.id = id
+        else:
+            self.__class__.__nb_objects += 1
+            self.id = self.__class__.__nb_objects
